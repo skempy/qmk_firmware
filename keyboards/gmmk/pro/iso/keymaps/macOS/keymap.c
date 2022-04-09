c445c6ab8bdf253d1b717f2f8214724522cd60f7
skempy <scottkemp28@gmail.com>
2022-04-09 18:29:24 +0100

    added visual aid for keymaps

--- keyboards/gmmk/pro/iso/keymaps/macOS/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/macOS/keymap.c
@@ -19,14 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Screenshot        Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
-//      Sh_L     /        Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Ct_L     Opt    CMD                               SPACE                          Search   FN       FN     Left     Down     Right
-
-
     // The FN key by default maps to a momentary toggle to layer 1 to provide access to the RESET key (to put the board into bootloader mode). Without
     // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
     // cable to get the board into bootloader mode - definitely not fun when you're working on your QMK builds. Remove this and put it back to KC_RGUI
@@ -38,19 +30,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
+
+    /* Base Layout
+     *
+     * ,-------------------------------------------------------------------------------------------------------------.
+     * | Esc  ||  F1  |  F2  |  F3  |  F4  ||  F5  |  F6  |  F7  |  F8  ||  F9  | F10  | F11  | F12  ||ScrSht|| Mute |
+     * |=============================================================================================================|
+     * |  ` ~ |  1 ! |  2 @ |  3 # |  4 $ |  5 % |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _ |  = + |  Backspc || Del  |
+     * |------+------+------+------+------+------+------+------+------+------+------+------+------+----------++------|
+     * |   Tab   |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   | [ }  | ] }  | Enter || PgUp |
+     * |---------+------+------+------+------+------+------+------+------+------+------+------+------+-      ++------|
+     * |  Capslock  |  A   |  S   |  D   |  F  |  G   |  H   |  J   |  K   |  L   | ; :  | ' "  |  \ | |     || PgDn |
+     * |------------+------+------+------+-----+------+------+------+------+------+------+------|----+========+------|
+     * | LShift |  #  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   | , <  | . >  | / ?  | RShift ||  Up  || End  |
+     * |--------------+------+------+------+------+------+------+------+------+------+------+--+=====++------++======|
+     * |  Ctrl  |   Opt  |  CMD   |               Space                  |Search|  Esc |  FN  || Left | Down | Right |
+     * `------------------------------------------------------------------------------------------------------------'
+     */
+
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,           KC_F11,  KC_F12,    LGUI(LSFT(LCTL(KC_5))),    KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             KC_MINS, KC_EQL,    KC_BSPC,                   KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,             KC_LBRC, KC_RBRC,                              KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_QUOT, KC_NUHS,   KC_ENT,                    KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SLSH,            KC_RSFT, KC_UP,            KC_END,
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             RGUI(KC_SPC),     KC_ESC,  MO(1),     KC_LEFT, KC_DOWN,          KC_RGHT
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,           KC_F11,  KC_F12,    LSG(KC_5),          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             KC_MINS, KC_EQL,    KC_BSPC,            KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,             KC_LBRC, KC_RBRC,                       KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_QUOT, KC_NUHS,   KC_ENT,             KC_PGDN,
+        KC_LSFT, KC_HASH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SLSH,            KC_RSFT, KC_UP,     KC_END,
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             RGUI(KC_SPC),     KC_ESC,  MO(1),     KC_LEFT, KC_DOWN,   KC_RGHT
     ),
+
+    /* FN1 Layout
+     *
+     * ,-------------------------------------------------------------------------------------------------------------.
+     * | ____ || ____ | ____ | ____ | ____ || ____ | ____ | ____ | ____ || ____ | ____ | ____ | ____ || ____ || ____ |
+     * |=============================================================================================================|
+     * | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |  Reset   ||Format|
+     * |------+------+------+------+------+------+------+------+------+------+------+------+------+----------++------|
+     * |  ______ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |       ||Import|
+     * |---------+------+------+------+------+------+------+------+------+------+------+------+------+--     ++------|
+     * |  --------  | ____ | ____ | ____ | ____| ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |     || ____ |
+     * |------------+------+------+------+-----+------+------+------+------+------+------+------|----+========+------|
+     * | _____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ______ ||RGBMOD|| ____ |
+     * |--------------+------+------+------+------+------+------+------+------+------+------+--+=====++------++======|
+     * | _____ | _____  |  _____  |               _____                  | ____ | ____ | ____ |||RGBSPD|RGBRMD|RGBSPI|
+     * `------------------------------------------------------------------------------------------------------------'
+     */
 
     [1] = LAYOUT(
         _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            LAG(KC_L),
+        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   LCA(KC_O),
         _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
